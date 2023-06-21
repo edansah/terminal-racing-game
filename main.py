5690606762d1253b19fe56bd44c2c5dcb44ad4f8
Edwin Ansah <edwinansah@live.co.uk>
2023-06-21 01:00:37 +0100

    First commit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from racers import racer_list
+from racers import *
+
+
+def greeting():
+    print(f'=================================\n'
+          f'---------------------------------\n'
+          f'WELCOME TO WACKY RACERS: THE GAME\n'
+          f'---------------------------------\n'
+          f'=================================\n')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(len(racer_list))
+    greeting()
+
+
+
+
+
+
